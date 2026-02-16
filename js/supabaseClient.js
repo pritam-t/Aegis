ac4d13e5dd9d8563472611db002f1b16ada5705e
pritam-t <techworld1773@gmail.com>
2026-02-16 13:31:48 +0530

    added keys

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,7 @@
 
-const SUPABASE_URL = process.env.VITE_SUPABASE_URL;
-const SUPABASE_KEY = process.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = "https://stwizpiwwqzzggbeinjj.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0d2l6cGl3d3F6emdnYmVpbmpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNjA4NjksImV4cCI6MjA4NjYzNjg2OX0.kXH5Ab3w90Wddn45g7yBjfQ7yzAaYFHoGWue_waxrdA"
+;
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
